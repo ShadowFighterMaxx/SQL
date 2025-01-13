4323ef13e29ecf795e4232cc657718e4cf47e54f
ShadowFighterMaxx <1annavarapu.sridatta@gmail.com>
2025-01-13 20:05:29 +0530

    initial

---
+++ act1.sql
@@ -0,0 +1,10 @@
+create TABLE mytb(
+id int PRIMARY KEY,
+name varchar(100), 
+grade int
+
+);
+INSERT INTO mytb(id,name,grade) VALUES (21232, "Harshit.P", 7);
+INSERT INTO mytb(id,name,grade) VALUES (26788, "Harshita.P", 9);
+INSERT INTO mytb(id,name,grade) VALUES (878787, "Anoop.S", 2);
+SELECT * from mytb
